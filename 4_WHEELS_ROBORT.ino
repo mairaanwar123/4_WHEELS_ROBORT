f909c4bdc02ae0d63b7c48c1f6dcb409cdc6059c
Maira khan <78538532+mairaanwar123@users.noreply.github.com>
2021-02-04 17:36:40 +0500

    Add files via upload

---
+++ 4_WHEELS_ROBORT.ino
@@ -0,0 +1,105 @@
+#include<AFMotor.h>
+
+AF_DCMotor motor1(1, MOTOR12_1KHZ);
+AF_DCMotor motor2(2, MOTOR12_1KHZ);
+AF_DCMotor motor3(3, MOTOR34_1KHZ);
+AF_DCMotor motor4(4, MOTOR34_1KHZ);
+char commmand;
+
+void setup() {
+Serial.begin(9600);
+}
+
+void loop() {
+ 
+if(serial.available()>0);
+{
+  command = Serial.read();
+  stop();
+  Serial.println(command);
+  switch(command){
+    case 'F':
+    forward();
+    break;
+    case 'B':
+    back();
+    break;
+    case 'L':
+    left();
+    break;
+    case'R':
+    right();
+    break;
+  }
+  }
+}
+void forward()
+{
+  motor1.setSpeed(225);
+  motor1.run(FORWARD);
+  motor2.setSpeed(225);
+  motor2.run(FORWARD);
+  motor3.setSpeed(225);
+  motor3.run(FORWARD);
+  motor4.setSpeed(225);
+  motor4.run(FORWARD);
+}
+void backward(){
+   motor1.setSpeed(225);
+   motor1.run(BACKWARD);
+   motor2.setSpeed(225);
+   motor2.run(BACKWARD);
+   motor3.setSpeed(225);
+   motor3.run(BACKWARD);
+   motor4.setSpeed(225);
+   motor4.run(BACKWARD);
+}
+
+void left(){
+   motor1.setSpeed(225);
+   motor1.run(BACKWARD);
+   motor2.setSpeed(225);
+   motor2.run(FORWARD);
+   motor3.setSpeed(225);
+   motor3.run(FORWARD);
+   motor4.setSpeed(225);
+   motor4.run(BACKWARD);
+ }
+
+void right(){
+   motor1.setSpeed(225);
+   motor1.run(FORWARD);
+   motor2.setSpeed(225);
+   motor2.run(BACKWARD);
+   motor3.setSpeed(225);
+   motor3.run(BACKWARD);
+   motor4.setSpeed(225);
+   motor4.run(FORWARD);
+}
+
+void stop(){
+  motor1.setspeed(0);
+  motor1.run(RELEASE);
+  motor2.setspeed(0);
+  motor2.run(RELEASE);
+  motor3.setspeed(0);
+  motor3.run(RELEASE);
+  motor4.setspeed(0);
+  motor4.run(RELEASE);
+}
+  
+  
+  
+
+
+
+
+
+
+  
+
+
+
+  
+}
+ 
